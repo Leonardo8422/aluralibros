8611e977aa2c9e373e069d93709bd03117a09bf8
Leonardo Alarcon <alarcon842@hotmail.com>
2025-01-03 16:59:13 -0600

    Inicio de proyecto aluralibros

---
+++ src/main/java/com/aluralibros/aluralibros/AluralibrosApplication.java
@@ -0,0 +1,13 @@
+package com.aluralibros.aluralibros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluralibrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluralibrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aluralibros/aluralibros/AluralibrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluralibros.aluralibros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluralibrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
